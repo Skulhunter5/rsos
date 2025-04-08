ef8e0e0f99e73ba3f5092530732069bf72cdc820
Skulhunter5 <linus.huster@gmail.com>
2025-04-08 10:59:14 +0200

    Add more pci device classes

--- src/pci/SubClass.rs
+++ src/pci/SubClass.rs
@@ -1,3 +1,10 @@
+pub struct Unclassified;
+
+impl Unclassified {
+    pub const NON_VGA_COMPATIBLE_UNCLASSIFIED_DEVICE: u8 = 0x0;
+    pub const VGA_COMPATIBLE_UNCLASSIFIED_DEVICE: u8 = 0x1;
+}
+
 pub struct MassStorageController;
 
 impl MassStorageController {
@@ -10,5 +17,64 @@ impl MassStorageController {
     pub const SERIAL_ATA_CONTROLLER: u8 = 0x6;
     pub const SERIAL_ATTACHED_SCSI_CONTROLLER: u8 = 0x7;
     pub const NON_VOLATILE_MEMORY_CONTROLLER: u8 = 0x8;
+    pub const OTHER: u8 = 0x80;
+}
+
+pub struct NetworkController;
+
+impl NetworkController {
+    pub const ETHERNET_CONTROLLER: u8 = 0x0;
+    pub const TOKEN_RING_CONTROLLER: u8 = 0x1;
+    pub const FDDI_CONTROLLER: u8 = 0x2;
+    pub const ATM_CONTROLLER: u8 = 0x3;
+    pub const ISDN_CONTROLLER: u8 = 0x4;
+    pub const WORLD_FIP_CONTROLLER: u8 = 0x5;
+    pub const PICMG_214_MULTI_COMPUTING_CONTROLLER: u8 = 0x6;
+    pub const INFINIBAND_CONTROLLER: u8 = 0x7;
+    pub const FABRIC_CONTROLLER: u8 = 0x8;
+    pub const OTHER: u8 = 0x80;
+}
+
+pub struct DisplayController;
+
+impl DisplayController {
+    pub const VGA_COMPATIBLE_CONTROLLER: u8 = 0x0;
+    pub const XGA_CONTROLLER: u8 = 0x1;
+    pub const CONTROLLER_3D: u8 = 0x2;
+    pub const OTHER: u8 = 0x80;
+}
+
+pub struct MultimediaController;
+
+impl MultimediaController {
+    pub const MULTIMEDIA_VIDEO_CONTROLLER: u8 = 0x0;
+    pub const MULTIMEDIA_AUDIO_CONTROLLER: u8 = 0x1;
+    pub const COMPUTER_TELEPHONY_DEVICE: u8 = 0x2;
+    pub const AUDIO_DEVICE: u8 = 0x3;
+    pub const OTHER: u8 = 0x80;
+}
+
+pub struct MemoryController;
+
+impl MemoryController {
+    pub const RAM_CONTROLLER: u8 = 0x0;
+    pub const FLASH_CONTROLLER: u8 = 0x1;
+    pub const OTHER: u8 = 0x80;
+}
+
+pub struct Bridge;
+
+impl Bridge {
+    pub const HOST_BRIDGE: u8 = 0x0;
+    pub const ISA_BRIDGE: u8 = 0x1;
+    pub const EISA_BRIDGE: u8 = 0x2;
+    pub const MCA_BRIDGE: u8 = 0x3;
+    pub const PCI_TO_PCI_BRIDGE: u8 = 0x4;
+    pub const PCMCIA_BRIDGE: u8 = 0x5;
+    pub const NUBUS_BRIDGE: u8 = 0x6;
+    pub const CARDBUS_BRIDGE: u8 = 0x7;
+    pub const RACEWAY_BRIDGE: u8 = 0x8;
+    pub const SEMI_TRANSPARENT_PCI_TO_PCI_BRIDGE: u8 = 0x9;
+    pub const INFINIBAND_TO_PCI_HOST_BRIDGE: u8 = 0x0A;
     pub const OTHER: u8 = 0x80;
 }
--- src/pci/type.rs
+++ src/pci/type.rs
@@ -1,16 +1,14 @@
 use super::SubClass;
 
-pub struct ClassCode;
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DeviceType {
     Unclassified { vga_compatible: bool },
     MassStorageController(MassStorageControllerType),
-    NetworkController,
-    DisplayController,
-    MultimediaController,
-    MemoryController,
-    Bridge,
+    NetworkController(NetworkControllerType),
+    DisplayController(DisplayControllerType),
+    MultimediaController(MultimediaControllerType),
+    MemoryController(MemoryControllerType),
+    Bridge(BridgeType),
     SimpleCommunicationController,
     BaseSystemPeripheral,
     InputDeviceController,
@@ -32,12 +30,12 @@ impl DeviceType {
     pub fn try_from(class_code: u8, subclass_code: u8, prog_if: u8) -> Result<Self, (u8, u8, u8)> {
         match class_code {
             ClassCode::UNCLASSIFIED => match subclass_code {
-                SubclassUnclassified::NON_VGA_COMPATIBLE_UNCLASSIFIED_DEVICE => {
+                SubClass::Unclassified::NON_VGA_COMPATIBLE_UNCLASSIFIED_DEVICE => {
                     Ok(Self::Unclassified {
                         vga_compatible: false,
                     })
                 }
-                SubclassUnclassified::VGA_COMPATIBLE_UNCLASSIFIED_DEVICE => {
+                SubClass::Unclassified::VGA_COMPATIBLE_UNCLASSIFIED_DEVICE => {
                     Ok(Self::Unclassified {
                         vga_compatible: true,
                     })
@@ -102,7 +100,7 @@ impl DeviceType {
                 SubClass::MassStorageController::SERIAL_ATA_CONTROLLER => {
                     if let Some(interface) = match prog_if {
                         0x0 => Some(SataControllerInterface::VendorSpecific),
-                        0x1 => Some(SataControllerInterface::AHCI),
+                        0x1 => Some(SataControllerInterface::Ahci),
                         0x2 => Some(SataControllerInterface::SerialStorageBus),
                         _ => None,
                     } {
@@ -113,16 +111,132 @@ impl DeviceType {
                         Err((class_code, subclass_code, prog_if))
                     }
                 }
-                SubClass::MassStorageController::SERIAL_ATTACHED_SCSI_CONTROLLER => todo!(),
-                SubClass::MassStorageController::NON_VOLATILE_MEMORY_CONTROLLER => todo!(),
-                SubClass::MassStorageController::OTHER => todo!(),
+                SubClass::MassStorageController::SERIAL_ATTACHED_SCSI_CONTROLLER => {
+                    if let Some(interface) = match prog_if {
+                        0x0 => Some(SasControllerInterface::Sas),
+                        0x1 => Some(SasControllerInterface::SerialStorageBus),
+                        _ => None,
+                    } {
+                        Ok(Self::MassStorageController(
+                            MassStorageControllerType::SasController { interface },
+                        ))
+                    } else {
+                        Err((class_code, subclass_code, prog_if))
+                    }
+                }
+                SubClass::MassStorageController::NON_VOLATILE_MEMORY_CONTROLLER => {
+                    if let Some(interface) = match prog_if {
+                        0x1 => Some(NvmControllerInterface::Nvmhci),
+                        0x2 => Some(NvmControllerInterface::Nvme),
+                        _ => None,
+                    } {
+                        Ok(Self::MassStorageController(
+                            MassStorageControllerType::NvmController { interface },
+                        ))
+                    } else {
+                        Err((class_code, subclass_code, prog_if))
+                    }
+                }
+                SubClass::MassStorageController::OTHER => Ok(Self::MassStorageController(MassStorageControllerType::Other)),
                 _ => Err((class_code, subclass_code, prog_if)),
             },
-            ClassCode::NETWORK_CONTROLLER => Ok(Self::NetworkController),
-            ClassCode::DISPLAY_CONTROLLER => Ok(Self::DisplayController),
-            ClassCode::MULTIMEDIA_CONTROLLER => Ok(Self::MultimediaController),
-            ClassCode::MEMORY_CONTROLLER => Ok(Self::MemoryController),
-            ClassCode::BRIDGE => Ok(Self::Bridge),
+            ClassCode::NETWORK_CONTROLLER => match subclass_code {
+                SubClass::NetworkController::ETHERNET_CONTROLLER => Ok(Self::NetworkController(NetworkControllerType::EthernetController)),
+                SubClass::NetworkController::TOKEN_RING_CONTROLLER => Ok(Self::NetworkController(NetworkControllerType::TokenRingController)),
+                SubClass::NetworkController::FDDI_CONTROLLER => Ok(Self::NetworkController(NetworkControllerType::FddiController)),
+                SubClass::NetworkController::ATM_CONTROLLER => Ok(Self::NetworkController(NetworkControllerType::AtmController)),
+                SubClass::NetworkController::ISDN_CONTROLLER => Ok(Self::NetworkController(NetworkControllerType::IsdnController)),
+                SubClass::NetworkController::WORLD_FIP_CONTROLLER => Ok(Self::NetworkController(NetworkControllerType::WorldFipController)),
+                SubClass::NetworkController::PICMG_214_MULTI_COMPUTING_CONTROLLER => Ok(Self::NetworkController(NetworkControllerType::Picmg214MultiComputingController)),
+                SubClass::NetworkController::INFINIBAND_CONTROLLER => Ok(Self::NetworkController(NetworkControllerType::InfinibandController)),
+                SubClass::NetworkController::FABRIC_CONTROLLER => Ok(Self::NetworkController(NetworkControllerType::FabricController)),
+                SubClass::NetworkController::OTHER => Ok(Self::NetworkController(NetworkControllerType::Other)),
+                _ => Err((class_code, subclass_code, prog_if)),
+            }
+            ClassCode::DISPLAY_CONTROLLER => match subclass_code {
+                SubClass::DisplayController::VGA_COMPATIBLE_CONTROLLER => {
+                    if let Some(ty) = match prog_if {
+                        0x0 => Some(VgaCompatibleControllerType::VgaController),
+                        0x1 => Some(VgaCompatibleControllerType::Controller8514Compatible),
+                        _ => None,
+                    } {
+                        Ok(Self::DisplayController(
+                            DisplayControllerType::VgaCompatibleController(ty)
+                        ))
+                    } else {
+                        Err((class_code, subclass_code, prog_if))
+                    }
+                }
+                SubClass::DisplayController::XGA_CONTROLLER => Ok(Self::DisplayController(DisplayControllerType::XgaController)),
+                SubClass::DisplayController::CONTROLLER_3D => Ok(Self::DisplayController(DisplayControllerType::Controller3D)),
+                SubClass::DisplayController::OTHER => Ok(Self::DisplayController(DisplayControllerType::Other)),
+                _ => Err((class_code, subclass_code, prog_if)),
+            }
+            ClassCode::MULTIMEDIA_CONTROLLER => match subclass_code {
+                SubClass::MultimediaController::MULTIMEDIA_VIDEO_CONTROLLER => Ok(Self::MultimediaController(MultimediaControllerType::MultimediaVideoController)),
+                SubClass::MultimediaController::MULTIMEDIA_AUDIO_CONTROLLER => Ok(Self::MultimediaController(MultimediaControllerType::MultimediaAudioController)),
+                SubClass::MultimediaController::COMPUTER_TELEPHONY_DEVICE => Ok(Self::MultimediaController(MultimediaControllerType::ComputerTelephonyDevice)),
+                SubClass::MultimediaController::AUDIO_DEVICE => Ok(Self::MultimediaController(MultimediaControllerType::AudioDevice)),
+                SubClass::MultimediaController::OTHER => Ok(Self::MultimediaController(MultimediaControllerType::Other)),
+                _ => Err((class_code, subclass_code, prog_if)),
+            }
+            ClassCode::MEMORY_CONTROLLER => match subclass_code {
+                SubClass::MemoryController::RAM_CONTROLLER => Ok(Self::MemoryController(MemoryControllerType::RamController)),
+                SubClass::MemoryController::FLASH_CONTROLLER => Ok(Self::MemoryController(MemoryControllerType::FlashController)),
+                SubClass::MemoryController::OTHER => Ok(Self::MemoryController(MemoryControllerType::Other)),
+                _ => Err((class_code, subclass_code, prog_if)),
+            }
+            ClassCode::BRIDGE => match subclass_code {
+                SubClass::Bridge::HOST_BRIDGE => Ok(Self::Bridge(BridgeType::HostBridge)),
+                SubClass::Bridge::ISA_BRIDGE => Ok(Self::Bridge(BridgeType::IsaBridge)),
+                SubClass::Bridge::EISA_BRIDGE => Ok(Self::Bridge(BridgeType::EisaBridge)),
+                SubClass::Bridge::MCA_BRIDGE => Ok(Self::Bridge(BridgeType::McaBridge)),
+                SubClass::Bridge::PCI_TO_PCI_BRIDGE => {
+                    if let Some(decode_mode) = match prog_if {
+                        0x0 => Some(PciBridgeDecodeMode::NormalDecode),
+                        0x1 => Some(PciBridgeDecodeMode::SubtractiveDecode),
+                        _ => None,
+                    } {
+                        Ok(Self::Bridge(
+                            BridgeType::PciToPciBridge { decode_mode }
+                        ))
+                    } else {
+                        Err((class_code, subclass_code, prog_if))
+                    }
+                }
+                SubClass::Bridge::PCMCIA_BRIDGE => Ok(Self::Bridge(BridgeType::PcmciaBridge)),
+                SubClass::Bridge::NUBUS_BRIDGE => Ok(Self::Bridge(BridgeType::NuBusBridge)),
+                SubClass::Bridge::CARDBUS_BRIDGE => Ok(Self::Bridge(BridgeType::CardBusBridge)),
+                SubClass::Bridge::RACEWAY_BRIDGE => {
+                    if let Some(mode) = match prog_if {
+                        0x0 => Some(RaceWayBridgeMode::Transparent),
+                        0x1 => Some(RaceWayBridgeMode::Endpoint),
+                        _ => None,
+                    } {
+                        Ok(Self::Bridge(
+                            BridgeType::RaceWayBridge { mode }
+                        ))
+                    } else {
+                        Err((class_code, subclass_code, prog_if))
+                    }
+                }
+                SubClass::Bridge::SEMI_TRANSPARENT_PCI_TO_PCI_BRIDGE => {
+                    if let Some(cpu_facing_bus) = match prog_if {
+                        0x40 => Some(CpuFacingBus::Primary),
+                        0x80 => Some(CpuFacingBus::Secondary),
+                        _ => None,
+                    } {
+                        Ok(Self::Bridge(
+                            BridgeType::SemiTransparentPciToPciBridge { cpu_facing_bus }
+                        ))
+                    } else {
+                        Err((class_code, subclass_code, prog_if))
+                    }
+                }
+                SubClass::Bridge::INFINIBAND_TO_PCI_HOST_BRIDGE => Ok(Self::Bridge(BridgeType::InfinibandToPciHostBridge)),
+                SubClass::Bridge::OTHER => Ok(Self::Bridge(BridgeType::Other)),
+                _ => Err((class_code, subclass_code, prog_if)),
+            }
             ClassCode::SIMPLE_COMMUNICATION_CONTROLLER => Ok(Self::SimpleCommunicationController),
             ClassCode::BASE_SYSTEM_PERIPHERAL => Ok(Self::BaseSystemPeripheral),
             ClassCode::INPUT_DEVICE_CONTROLLER => Ok(Self::InputDeviceController),
@@ -145,6 +259,8 @@ impl DeviceType {
         }
     }
 }
+
+pub struct ClassCode;
 
 impl ClassCode {
     pub const UNCLASSIFIED: u8 = 0x0;
@@ -171,14 +287,7 @@ impl ClassCode {
     pub const UNASSIGNED_CLASS: u8 = 0xFF;
 }
 
-pub struct SubclassUnclassified;
-
-impl SubclassUnclassified {
-    pub const NON_VGA_COMPATIBLE_UNCLASSIFIED_DEVICE: u8 = 0x0;
-    pub const VGA_COMPATIBLE_UNCLASSIFIED_DEVICE: u8 = 0x1;
-}
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum MassStorageControllerType {
     ScsiBusController,
     IdeController {
@@ -196,23 +305,126 @@ pub enum MassStorageControllerType {
     SataController {
         interface: SataControllerInterface,
     },
+    SasController {
+        interface: SasControllerInterface,
+    },
+    NvmController {
+        interface: NvmControllerInterface,
+    },
+    Other,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum IdeControllerMode {
     IsaCompatibilityMode,
     PciNativeMode,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum AtaControllerDmaMode {
     SingleDma,
     ChainedDma,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum SataControllerInterface {
     VendorSpecific,
-    AHCI,
+    Ahci,
     SerialStorageBus,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum SasControllerInterface {
+    Sas,
+    SerialStorageBus,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum NvmControllerInterface {
+    Nvmhci,
+    Nvme,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum NetworkControllerType {
+    EthernetController,
+    TokenRingController,
+    FddiController,
+    AtmController,
+    IsdnController,
+    WorldFipController,
+    Picmg214MultiComputingController,
+    InfinibandController,
+    FabricController,
+    Other,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum DisplayControllerType {
+    VgaCompatibleController(VgaCompatibleControllerType),
+    XgaController,
+    Controller3D,
+    Other,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum VgaCompatibleControllerType {
+    VgaController,
+    Controller8514Compatible,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum MultimediaControllerType {
+    MultimediaVideoController,
+    MultimediaAudioController,
+    ComputerTelephonyDevice,
+    AudioDevice,
+    Other,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum MemoryControllerType {
+    RamController,
+    FlashController,
+    Other,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BridgeType {
+    HostBridge,
+    IsaBridge,
+    EisaBridge,
+    McaBridge,
+    PciToPciBridge {
+        decode_mode: PciBridgeDecodeMode,
+    },
+    PcmciaBridge,
+    NuBusBridge,
+    CardBusBridge,
+    RaceWayBridge {
+        mode: RaceWayBridgeMode,
+    },
+    SemiTransparentPciToPciBridge {
+        cpu_facing_bus: CpuFacingBus,
+    },
+    InfinibandToPciHostBridge,
+    Other,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PciBridgeDecodeMode {
+    NormalDecode,
+    SubtractiveDecode,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum RaceWayBridgeMode {
+    Transparent,
+    Endpoint,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum CpuFacingBus {
+    Primary,
+    Secondary,
 }
