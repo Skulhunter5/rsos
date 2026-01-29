59c29d7ef6129678879bf71a1793713d43dd793c
Skulhunter5 <linus.huster@gmail.com>
2026-01-29 17:54:01 +0100

    Fix things for new rust version

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -4,7 +4,6 @@
 #![feature(ptr_mask)]
 #![feature(pointer_is_aligned_to)]
 #![feature(slice_ptr_get)]
-#![feature(new_zeroed_alloc)]
 
 #[cfg(not(target_pointer_width = "64"))]
 compile_error!("unsupported target pointer width");
